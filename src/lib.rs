f7231e556e385052376b5e1b715ecea011d18e57
markmehere <52765023+markmehere@users.noreply.github.com>
2024-07-04 15:50:38 +1000

    Ran cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@
 use anyhow::{anyhow, bail, Context, Result};
 use fs4::FileExt;
 use siphasher::sip::SipHasher13;
-use ureq::Response;
 use std::collections::HashSet;
 use std::env;
 use std::fs;
@@ -12,6 +11,7 @@ use std::hash::{Hash, Hasher};
 use std::io;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
+use ureq::Response;
 
 /// Global cache for wasm-pack, currently containing binaries downloaded from
 /// urls like wasm-bindgen and such.
@@ -393,14 +393,14 @@ impl Download {
 fn download_binary(url: &str) -> Result<Vec<u8>> {
     let use_native_tls = env::var("USE_NATIVE_TLS").ok();
     let response: Response;
-    
+
     if use_native_tls.as_deref() == Some("1") {
         response = ureq::AgentBuilder::new()
             .tls_connector(Arc::new(native_tls::TlsConnector::new()?))
             .build()
-            .get(url).call()?;
-    }
-    else {
+            .get(url)
+            .call()?;
+    } else {
         response = ureq::get(url).call()?;
     }
 
